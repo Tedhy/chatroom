6d14c0215f5949914a869db394c15ff64db9277d
Tedhy <mss.tgarcia@gmail.com>
2012-01-30 21:43:40 -0500

    firs

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chatroom::Application.config.secret_token = '6cf43688e53059e21f706a32b225632c6643fa865ab780e22223c8659b33786af0b1035d9016d9aead1329cc9a9fff659415644f03930d1713573286659b51ba'
